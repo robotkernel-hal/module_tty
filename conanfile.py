20d569d29834f5cbd2ffd0f24818976e1a129660
Robert Burger <robert.burger@dlr.de>
2026-05-22 08:59:22 +0200

    update: conanfile and Makefile path

--- conanfile.py
+++ conanfile.py
@@ -1,19 +1,15 @@
 from conan import ConanFile
 
 class MainProject(ConanFile):
-    python_requires = "conan_template/[~5]@robotkernel/stable"
+    python_requires = "conan_template/[~6]@robotkernel/stable"
     python_requires_extend = "conan_template.RobotkernelConanFile"
 
     name = "module_tty"
     description = ""
     exports_sources = ["*", "!.gitignore"]
     requires = (
-        "robotkernel/[~6]@robotkernel/unstable",
-        "service_provider_process_data_inspection/[~6]@robotkernel/unstable",
-        "service_provider_memory_inspection/[~6]@robotkernel/unstable",
+        "robotkernel/[~6]@robotkernel/stable",
+        "service_provider_process_data_inspection/[~6]@robotkernel/stable",
+        "service_provider_memory_inspection/[~6]@robotkernel/stable",
     )
-
-    
-    def source(self):
-        self.run(f"sed 's/AC_INIT(.*/AC_INIT([module_tty], [{self.version}], [{self.author}])/' configure.ac.in > configure.ac")
 
